633fb02f7d5594057c51e32d5fec1a29a3c31edf
Cecilia <cecilia.delacamara@gmail.com>
2025-08-04 20:41:32 +0200

    funcion altaCliente

--- src/main/java/pajareria/GestionClientes.java
+++ src/main/java/pajareria/GestionClientes.java
@@ -16,4 +16,20 @@ import java.util.Scanner;
 public class GestionClientes {
     private ArrayList<Cliente> clientes = new ArrayList<>();
     private Scanner scanner = new Scanner(System.in);
+
+    // Funcion dar de alta cliente
+
+    public void altaCliente(){
+        System.out.println("Nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("DNI: ");
+        String dni = scanner.nextLine();
+        System.out.println("Teléfono: ");
+        String telefono = scanner.nextLine();
+        System.out.println("Email: ");
+        String email = scanner.nextLine();
+
+        clientes.add(new Cliente(nombre, dni, telefono, email)); // Se crea un objeto cliente con los datos que el usuario ha escrito
+        System.out.println("Cliente añadido con éxito.");
+    }
 }
